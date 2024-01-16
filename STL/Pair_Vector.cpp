431697abb8d8c1007a98196c5fb6764623bb96d1
Habibur Rahman <hr13338@gmail.com>
2024-01-16 18:01:22 +0600

    first commit

---
+++ STL/Pair_Vector.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"good to go ";
+    pair<int , string > p;
+    p = { 12 , "habibi"} ;
+    cout<<p.first << " "<< p.second ;
+
+    return 0;
+}
